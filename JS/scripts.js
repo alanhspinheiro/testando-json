a221da1bdb1ef725ad0d7ae74d1286be2f49aaf6
alanhspinheiro <alanhspinheiro@gmail.com>
2022-03-28 12:22:02 -0300

    adicionado o arquivo JSON, o arquivo js e o arquivo html

---
+++ JS/scripts.js
@@ -0,0 +1,50 @@
+const objs = [
+    {
+    nome: 'Alan',
+    idade: 30,
+    esta_trabalhando: true,
+    hobbies: ['música', 'games', 'pedaladas'],
+    detalhes_profissão: {
+        profissão: 'rogramador',
+        empresa: 'empresa x'
+
+    }
+
+
+},
+{
+    nome: 'João',
+    idade: 35,
+    esta_trabalhando: false,
+    hobbies: ['música', 'corrida', 'futebol'],
+    detalhes_profissão: {
+        profissão: null,
+        empresa: null
+
+    }
+
+
+}
+]
+console.log(objs)
+
+
+//JSON
+//CONVERTER OBJETO PARA JSON
+const jsonData = JSON.stringify(objs)
+
+console.log(jsonData)
+console.log(typeof jsonData)
+
+//Converter JSON para objeto
+const objData = JSON.parse(jsonData); 
+
+console.log(objData)
+console.log(typeof objData)
+
+objData.map((pessoa) =>
+console.log(pessoa.nome)
+
+)
+
+
